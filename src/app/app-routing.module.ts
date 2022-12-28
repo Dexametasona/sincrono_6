61ed8d6eceb29e719ecab9f464b0ae2817f51e80
Dexametasona <machicadogomezalexander@gmail.com>
2022-12-28 09:15:59 -0500

    configuracion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { InicioComponent } from './modulos/usuarios/inicio/inicio.component';
+import { ContactoComponent } from './modulos/usuarios/contacto/contacto.component';
+import { ServiciosComponent } from './modulos/usuarios/servicios/servicios.component';
+import { NosotrosComponent } from './modulos/usuarios/nosotros/nosotros.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Inicio', component: InicioComponent},
+  {path:'**', pathMatch:'full', redirectTo:'Inicio'},
+  {path:'Nosotros', component: NosotrosComponent},
+  {path:'Servicios', component: ServiciosComponent},
+  {path:'Contactos', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
